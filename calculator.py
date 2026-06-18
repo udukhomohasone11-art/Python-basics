b32f42fb56363192b933c2283c0372908121e660
udukhomohasone11-art <udukhomohasone11@gmail.com>
2026-06-18 13:51:30 +0100

    Create calculator.py
    calculator project

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+Simple Calculator for Mercor
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+print("Test: 5 + 3 =", add(5, 3))
+print("Test: 10 - 4 =", subtract(10, 4))
